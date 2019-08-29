2a75872307c8c96b9d9016518d9d07d1342796ae
suvarchal <suvarchal.kumar@gmail.com>
2019-08-30 00:16:03 +0200

    update locking module

--- gar/lock.py
+++ gar/lock.py
@@ -2,40 +2,42 @@ from contextlib import contextmanager
 import os
 from pathlib import Path
 import fcntl
-__lockpath__ = "/var/lock"
 
-# check if path exists else put it in tmp?
-# test if able to lock?
+
+lockpath = Path("/var/lock")
+
+if lockpath.exists() and os.access(lockpath, os.W_OK):
+    lockpath = lockpath
+else:
+    lockpath = Path.home()
 
 class SimpleFileLock:
-    def __init__(self,lockfile):
-        self.lockfile = lockfile
+    def __init__(self, lockfile):
+        self.lockfile = lockpath / lockfile
+
     def __enter__(self):
         if os.path.exists(self.lockfile):
-            raise OSError("Another gar process running for same group")
+            raise OSError("Cannot acquire lock: Another process running")
         try:
             os.open(self.lockfile, os.O_CREAT, 0o640)
         except OSError as e:
-            SystemExit(e)
-        
-    def __exit__(self, exc_type, exc_val, exc_tb): 
+            raise SystemExit(e)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
         os.remove(self.lockfile)
 
 class FileLock:
+    """ doesn't work perfectly yet
+    """
     def __init__(self, lockfile):
-        self.lockfile = lockfile
+        self.lockfile = lockpath / lockfile
     def __enter__(self):
-        os.open(file_n,os.O_CREAT,0o600)
         try:
-            fcntl.flock(lock_fd,fcntl.LOCK_EX | fcntl.LOCK_NB)
-            locked = True
-        except IOError:
-             print("failed")
+            lock_fd = os.open(self.lockfile, os.O_CREAT, 0o640)
+            fcntl.flock(lock_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except (IOError,OSError) as e:
+            os.close(lock_fd)
+            raise OSError(e) #SystemExit(e)
     def __exit__(self, exc_type, exc_val, exc_tb):
-
-        try:
-           os.close(lock_fd)
-           os.unlink(file_n)
-           print("successfully released lock")
-        except:
-           print("failed to release lock, clean manually") 
+        #os.close(lock_fd)
+        os.remove(self.lockfile)
