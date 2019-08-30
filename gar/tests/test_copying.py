06ab0c7bab54732381cbb2fe2e6f694d97b3a3f3
suvarchal <suvarchal.kumar@gmail.com>
2019-08-30 03:01:31 +0200

    fix indentation for CI

--- gar/tests/test_copying.py
+++ gar/tests/test_copying.py
@@ -2,30 +2,3 @@ import pytest
 import os
 #from .copy import copy
 
-class TestCopy:
-    #def setup_class(self):
-    #    print("setup")
-
-    #def teardown_method(self):
-    #    print("teardown")
-    def test_copy(self):
-        print('hello')
-        assert 1
-    @pytest.mark.multiuser
-    def test_copy_whatever(self):
-        print("in multiuser")
-        assert 1
-    def test_new(self,setup):
-        print("hello in proces")
-        assert 1
-    def test_new2(self,setup):
-        print("heello in 2")
-        assert 1
- #   def test_copyit(self):
- #       print('done copy')
- #       assert 0
-
-#@pytest.mark.slow
-#def test_none():
-#    print("hello")
-     assert 0
