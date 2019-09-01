0f31cf1cb9456cc4ebe1c271be7682a7ff5563e0
suvarchal <suvarchal.kumar@gmail.com>
2019-09-02 01:47:32 +0200

    remove git changelog before bump up version

--- ci/build_debian.sh
+++ ci/build_debian.sh
@@ -1,14 +1,15 @@
 # install dependencies
-sudo apt-get update -q
-sudo apt-get install fakeroot -y 
-sudo apt-get install debhelper -y
-sudo apt-get install dh-python -y
-sudo apt-get install devscripts -y
-sudo apt-get install python3 -y
-sudo apt-get install python3-setuptools -y
-sudo apt-get install python3-pytest -y
+#sudo apt-get update -q
+#sudo apt-get install fakeroot -y 
+#sudo apt-get install debhelper -y
+#sudo apt-get install dh-python -y
+#sudo apt-get install devscripts -y
+#sudo apt-get install python3 -y
+#sudo apt-get install python3-setuptools -y
+#sudo apt-get install python3-pytest -y
 
 # change changelog
+rm debian/changelog
 export DEBEMAIL="suvarchal.kumar@gmail.com"
 export DEBFULLNAME="Suvarchal K. Cheedela"
 dch --package "gar" --create -v ${TRAVIS_TAG#"v"} -D stable "upstream release"
