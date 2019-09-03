6bbeeb9124de8ecb2c057670a0acc7d855e8e1c0
suvarchal <suvarchal.kumar@gmail.com>
2019-09-03 18:20:41 +0200

    add debian functional full test

--- ci/test_debian.sh
+++ ci/test_debian.sh
@@ -2,10 +2,10 @@ TAG=${TRAVIS_TAG#"v"}
 cwd=`pwd`
 cd ci
 cp ../../python3-gar_${TAG}_all.deb python3-gar_${TAG}_all.deb
-#docker build --build-arg TAG=${TAG} --tag=buster . --target=buster
+docker build --build-arg TAG=${TAG} --tag=buster . --target=buster
 
-#docker build --build-arg TAG=${TAG} --tag=bullseye . --target=bullseye 
+docker build --build-arg TAG=${TAG} --tag=bullseye . --target=bullseye 
 
-#docker build --build-arg TAG=${TAG} --tag=bionic . --target=bionic
+docker build --build-arg TAG=${TAG} --tag=bionic . --target=bionic
 
 cd $cwd
