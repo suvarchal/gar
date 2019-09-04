543f094bf6092fd971062b85abdc25a5e51d711b
suvarchal <suvarchal.kumar@gmail.com>
2019-09-04 23:35:51 +0200

    restructure pytest fixtures

--- gar/tests/conftest.py
+++ gar/tests/conftest.py
@@ -1,18 +1,63 @@
 import pytest
 import os
+import tempfile
+import shutil
 from pathlib import Path
-import random
-import string
 
 
-def create_dirs(numdirs=1, basedir="."):
-    for nd in range(numdirs):
-        if not Path(basedir).exists():
-            Path(basedir).mkdir()
-        dpath = "".join(random.choice(string.ascii_lowercase) 
-                        for x in range(5))
-        print(dpath)
-        (Path(basedir) / dpath).mkdir()
+@pytest.fixture(scope="function")
+def tempf():
+    """ Create a tempfile write 5 bytes
+    """
+    _, tempf = tempfile.mkstemp()
+    tempf = Path(tempf)
+    # make a temp file with 5 bytes
+    tempf.write_bytes(b"tempo")
+    yield tempf
+    tempf.unlink() if tempf.exists() else None
+
+@pytest.fixture(scope="function")
+def tempsym(tempf):
+    """ Create a symlink to a tempfile
+    note chaining fixtures tempsym and tempf
+    """
+    symf = Path("tempsymf")
+    os.symlink(tempf, symf)
+    yield symf, tempf
+    symf.unlink() if symf.exists() else None
+    tempf.unlink() if tempf.exists() else None
+
+@pytest.fixture(scope="function")
+def tempdir():
+    """ Create a tmp directory """
+    td = tempfile.mkdtemp()
+    td = Path(td)
+    yield td
+    shutil.rmtree(td) if td.exists() else None
+
+@pytest.fixture(scope="function")
+def tempdirwithfiles(tempdir):
+    """ tempdir
+            |-sdir/
+                |-tf2
+                |-renamedlink (relative symlink to ../tf1)
+            |-tf1 (5 bytes)
+            |-tf2 (symlink to td/tf2)
+    """
+    sdir = tempfile.mkdtemp(dir=tempdir)
+    _, tf1 = tempfile.mkstemp(dir=tempdir)
+    Path(tf1).write_bytes(b"tempo")
+    # a file inside td
+    _, tf2 = tempfile.mkstemp(dir=sdir)
+    # relative symlink
+    renamedlink_tf1 = Path(sdir) / "renamedlink_tf1"
+    os.symlink(f"../{Path(tf1).name}", renamedlink_tf1)
+    # abs symlink
+    link_tf2 = Path(tempdir)/Path(tf2).name
+    os.symlink(tf2, link_tf2)
+    yield (tempdir, sdir, tf1, tf2, renamedlink_tf1, link_tf2)
+    shutil.rmtree(tempdir) if Path(tempdir).exists() else None
+
 
 
 @pytest.fixture(scope='module')
--- gar/tests/test_misc.py
+++ gar/tests/test_misc.py
@@ -4,22 +4,10 @@ import tempfile
 import hashlib
 import shutil
 import pytest
+from functools import reduce
 from gar import utils
 from gar import lock
 from gar.logger import syslogpath, rotator, setup_logger
-
-
-# Fixture: Create a temp file
-@pytest.fixture
-def tempf():
-    """ Create a tempfile write 5 bytes
-    """
-    _, tempf = tempfile.mkstemp()
-    tempf = Path(tempf)
-    # make a temp file with 5 bytes
-    tempf.write_bytes(b"tempo")
-    yield tempf
-    tempf.unlink() if tempf.exists() else None
 
 
 # test group utils
@@ -74,60 +62,8 @@ def test_logger_rotator(temp_log):
     assert not temp_log[0].exists()
     assert temp_log[1].exists()
 
+
 # test file utils of gar.utils
-@pytest.fixture
-def tempf():
-    """ Create a tempfile write 5 bytes
-    """
-    _, tempf = tempfile.mkstemp()
-    tempf = Path(tempf)
-    # make a temp file with 5 bytes
-    tempf.write_bytes(b"tempo")
-    yield tempf
-    tempf.unlink() if tempf.exists() else None
-
-@pytest.fixture
-def tempsym(tempf):
-    """ Create a symlink to a tempfile
-    note chaining fixtures tempsym and tempf
-    """
-    symf = Path("tempsymf")
-    os.symlink(tempf, symf)
-    yield symf, tempf
-    symf.unlink() if symf.exists() else None
-
-@pytest.fixture
-def tempdir():
-    """ Create a tmp directory """
-    td = tempfile.mkdtemp()
-    td = Path(td)
-    yield td
-    shutil.rmtree(td) if td.exists() else None
-
-@pytest.fixture
-def tempdirwithfiles(tempdir):
-    """ tempdir
-            |-sdir/
-                |-tf2
-                |-renamedlink (relative symlink to ../tf1)
-            |-tf1 (5 bytes)
-            |-tf2 (symlink to td/tf2)
-    """
-    sdir = tempfile.mkdtemp(dir=tempdir)
-    _, tf1 = tempfile.mkstemp(dir=tempdir)
-    Path(tf1).write_bytes(b"tempo")
-    # a file inside td
-    _, tf2 = tempfile.mkstemp(dir=sdir)
-    # relative symlink
-    renamedlink_tf1 = Path(sdir) / "renamedlink_tf1"
-    os.symlink(f"../{Path(tf1).name}", renamedlink_tf1)
-    # abs symlink
-    link_tf2 = Path(tempdir)/Path(tf2).name
-    os.symlink(tf2, link_tf2)
-    yield (tempdir, sdir, tf1, tf2, renamedlink_tf1, link_tf2)
-    shutil.rmtree(tempdir) if Path(tempdir).exists() else None
-
-
 def test_filestat(tempf):
     """ checks utils.cp_filestat, utils.hr_size """
     assert utils.cp_filestat(tempf)['Size'] == tempf.stat().st_size
@@ -159,6 +95,7 @@ def test_filestat(tempf):
     assert utils.cp_linkstat(tempf) is None
     assert utils.cp_dirstat(tempf) is None
 
+    os.unlink(tempfcopy)
 
 def test_symlink(tempsym):
     symf, tempf = tempsym
@@ -173,7 +110,7 @@ def test_symlink(tempsym):
     assert lstat == lstat2
 
 
-def test_hash_utils(tempf, tempsym, tempdirwithfiles):
+def temp_hash_utils(tempf, tempsym, tempdirwithfiles):
     """ Check if hashing functions work
     as intended, very important for
     verifying integrity of copy/move
@@ -198,7 +135,6 @@ def test_hash_utils(tempf, tempsym, tempdirwithfiles):
     shutil.copytree(tempdirwithfiles[0], tempdcopy, symlinks=True)
     h2 = utils.hash_walk(tempdcopy)
     
-    from functools import reduce
     h1sum = reduce(lambda x, y: x ^ y, h1.values())
 
     h2sum = reduce(lambda x, y: x ^ y, h2.values())
